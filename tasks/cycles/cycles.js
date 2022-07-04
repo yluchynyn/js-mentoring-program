a015651177add227b5e45aee6a8a2826fa1102f9
yluchynyn <yevheniia_luchynyn@epam.com>
2022-07-04 13:06:11 -0400

    last commit

--- tasks/cycles/cycles.js
+++ tasks/cycles/cycles.js
@@ -8,26 +8,65 @@
 let forFactorial, whileFactorial, doFactorial;
 const n = 10; //10! = 3628800
 
+forFactorial=1;
+for(let i=n;i>0;i--){
+  forFactorial*=i;
+}
+console.log(forFactorial);
+
+
+whileFactorial=1;
+let i = n;
+while(i>0){
+  whileFactorial*=i;
+  i--;
+}
+console.log(whileFactorial);
+
+
+doFactorial=1;
+i = n;
+do{
+doFactorial*=i;
+i--;
+}while(i>0);
+console.log(doFactorial);
+
 /** Task: 2
  * return the concatenated string from an array of substring
  * assign the result to variable 'str'
  * hint: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...of
  */
-let str;
+let str="";
 const substr = ["I", " love", " JS"];
+
+for(let el of substr){
+  str+=el;
+}
+console.log(str);
 
 /** Task: 3
  * calculate a total of income of certain person
  * assign the result to the variable 'totalIncome'
  * hint: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...in
  */
-let totalIncome;
+let totalIncome=0;
 const personIncomes = {
   salary: 1985,
   rent: -600,
   interestOnDeposit: 250,
   otherExpences: -300
 };
+
+for(let key in personIncomes){
+  totalIncome+=personIncomes[key];
+}
+console.log(totalIncome);
+
+
+
+
+
 
 module.exports = {
   forFactorial,
