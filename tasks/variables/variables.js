48f634efbc58703a30e003e13cc0cc2c2d45d5cc
yluchynyn <yevheniia_luchynyn@epam.com>
2022-07-12 16:39:29 -0400

    var variable replaced with let variable

--- tasks/variables/variables.js
+++ tasks/variables/variables.js
@@ -1,5 +1,5 @@
 /**
- * Materials: 
+ * Materials:
  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Data_structures
  */
 
@@ -7,52 +7,51 @@
  * TODO create variable named int and set value equal to 1
  */
 
-var int = 1;
-//console.log(int);
+let int = 1;
+
 /**
  *  TODO create variable named int2 and set value equal int plus 1
  */
-var int2=int+1;
-//console.log(int2);
+let int2 = int + 1;
+
 /**
  * TODO create variable named str and set value equal to "John"
  */
-var str="John";
+let str = "John";
 /**
  * TODO create variable named str2 and set value equal str plus " Dou"
  */
-var str2=str+" Dou";
-//console.log(str2);
+let str2 = str + " Dou";
+
 /**
  * TODO create variable named bool and set value of equality comparison of int and int2 variables
  */
- var bool=int==int2;
-//console.log(bool);
+let bool = int == int2;
+
 /**
  * TODO create variable named arr and set value array of numbers from 1 to 5
  */
-var arr=[1,2,3,4,5];
-//console.log(arr);
+let arr = [1, 2, 3, 4, 5];
+
 /**
  * TODO create variable named fifth and set value as element of array with index 4
  */
-var fifth=arr[4];
-//console.log(fifth);
+var fifth = arr[4];
+
 /**
  * TODO create variable named human and set value as object with key firstName and value "John"
  */
-var human={firstName:"John"};
-//console.log(human);
+let human = { firstName: "John" };
+
 /**
  * TODO set new key of human object named lastName and value "Dou"
  */
- human.lastName="Dou";
- //console.log(human);
+human.lastName = "Dou";
+
 /**
  * TODO set new key of human object named fullName and value of concatenation of firstName and lastName values
  */
-  human.fullName=human.firstName+" "+human.lastName;
-//console.log(human);
+human.fullName = human.firstName + " " + human.lastName;
 
 module.exports = {
   int,
@@ -62,5 +61,5 @@ module.exports = {
   bool,
   arr,
   fifth,
-  human
+  human,
 };
