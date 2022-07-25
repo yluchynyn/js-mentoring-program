da09a3d731600f224b36e6274fa0ea02e4a8d362
yluchynyn <yevheniia_luchynyn@epam.com>
2022-07-25 13:34:00 -0400

    String tasks

--- tasks/strings/strings.js
+++ tasks/strings/strings.js
@@ -1,5 +1,5 @@
 /**
- * 1. Write a method to reverse a string; 
+ * 1. Write a method to reverse a string;
  * 2. The method should return the string 'This is not a string!' if trying to pass a number
  * hint: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reverse
  * @param {string} str
@@ -9,8 +9,14 @@
  * console.log(reverseString(123)) // 'This is not a string!'
  */
 function reverseString(str) {
-
+  let reversed = "";
+  for (let i = str.length; i >= 0; i--) {
+    reversed += str.charAt(i);
+  }
+  return reversed;
 }
+
+//console.log(reverseString('This is not a string!'));
 
 /**
  * Calculate a century by given year if a number is passed
@@ -23,8 +29,9 @@ function reverseString(str) {
  * hint: please choose and appropriate method from the Math object
  */
 function centuryFromYear(year) {
-
+  return Math.round(Math.ceil(year / 100));
 }
+//console.log(centuryFromYear(1601))
 
 /**
  * Calculate how many times the 'char' appears in the provided 'string'
@@ -38,12 +45,17 @@ function centuryFromYear(year) {
  * hint: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/match
  */
 function strCount(str, char) {
-
+  const re = new RegExp(char, "gi");
+  const arr = str.match(re);
+  if (arr === null) {
+    return 0;
+  }
+  return arr.length;
 }
-
+//console.log(strCount('oooooooOOl', 'o'))
 /**
  * Write a method to reduce the length of the string or truncate if it is longer
- * than the given maximum length and add "..." to the end. 
+ * than the given maximum length and add "..." to the end.
  * Keep it as is if it is not that long.
  * @param {string} str - the initial string
  * @param {num} num - by wht amount of chars it should be truncated
@@ -53,8 +65,10 @@ function strCount(str, char) {
  * console.log(truncateString('This', 6)) // 'This'
  */
 function truncateString(str, num) {
-
+  return str.slice(0, num);
 }
+// console.log(truncateString('This is a long string', 6))
+// console.log(truncateString('This', 6))
 
 /**
  * replace 10 with 'ten' word
@@ -65,8 +79,9 @@ function truncateString(str, num) {
  * hint: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace
  */
 function replace10(text) {
-
+  return text.replace(/10/g, "ten");
 }
+//console.log(replace10("231054"))
 
 /**
  * replace value in the square brackets with CONFIDENTIAL
@@ -76,8 +91,10 @@ function replace10(text) {
  * console.log(replaceConfidential("lorem [ipsum] si dolor")) // lorem [CONFIDENTIAL] si dolor
  */
 function replaceConfidential(text) {
-
+  const confidential = text.slice(text.indexOf("[") + 1, text.indexOf("]"));
+  return text.replace(confidential, "CONFIDENTIAL");
 }
+//console.log(replaceConfidential("lorem [ipsum] si dolor"))
 
 module.exports = {
   reverseString,
@@ -85,5 +102,5 @@ module.exports = {
   strCount,
   truncateString,
   replace10,
-  replaceConfidential
+  replaceConfidential,
 };
