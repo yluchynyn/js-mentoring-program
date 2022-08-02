9c3ea3ea4fe367cbeb492974e1e6e6bf8df6b41f
yluchynyn <yevheniia_luchynyn@epam.com>
2022-08-01 21:19:41 -0400

    First commit Objects

--- tasks/oop/objects.js
+++ tasks/oop/objects.js
@@ -3,19 +3,41 @@
  * create object person with firstName and lastName properties
  * implement method getFullName that returns full name
  */
-const person = ''; //put you object here
+const person = {
+	firstName : 'Jenna',
+	lastName : 'Luchynyn',
+	getFullName(){
+		console.log('${this.firstName} ${this.lastName}')
+	}
+}; //put you object here
 
+console.log(person);
+person.getFullName();
 /**
  * create object student that has property grade and method getGrade
  * set person as prototype of student
  */
-const student = ''; //put you object here
+const student = {
+	grade : 87,
+	getGrade(){
+		console.log('${this.grade}')
+	}} //put you object here
+
+Object.setPrototypeOf(student, person);
+// student.getFullName();
+// student.getGrade();
+// console.log(student);
+
 
 /**
  * create new instance of student using Object.create
  */
-const student2 = ''; //put you object here
-
+const student2 = Object.create(student,
+	{firstName:{value:"Alex"},
+	lastName:{value:'F'}
+	}); //put you object here
+// student2.getFullName();
+// console.log(student2);
 module.exports = {
 	person,
 	student,
